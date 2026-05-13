bd9b1800027a596cdb735ca93df350a776a890bc
SennaBelder <9010538@student.zadkine.nl>
2026-05-13 10:11:03 +0200

    Database

--- dbconnect.php
+++ dbconnect.php
@@ -1,16 +1,12 @@
-<?php 
-try 
-{ 
-    $db = new PDO('mysql:host=localhost;dbname=breadcompany', 'root' ,''); 
-} 
-catch(PDOException $e) 
-{ 
+<?php
+try {
+    $db = new PDO('mysql:host=localhost;dbname=The-Croods', 'root', '');
+} catch (PDOException $e) {
     $sMsg = '<p> 
-            Regelnummer: '.$e->getLine().'<br /> 
-            Bestand: '.$e->getFile().'<br /> 
-            Foutmelding: '.$e->getMessage().' 
-        </p>'; 
-     
-    trigger_error($sMsg); 
-} 
-?> 
+            Regelnummer: ' . $e->getLine() . '<br /> 
+            Bestand: ' . $e->getFile() . '<br /> 
+            Foutmelding: ' . $e->getMessage() . ' 
+        </p>';
+
+    trigger_error($sMsg);
+}
