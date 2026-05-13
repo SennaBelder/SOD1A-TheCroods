174a49cbb3f09c9deb708edf3a6008ef9c155511
jayden-rezigue <9029068@student.zadkine.nl>
2026-05-13 10:43:37 +0200

    database

--- scripts/update-passwrd.php
+++ scripts/update-passwrd.php
@@ -9,7 +9,7 @@
     </head>
     <body>
         <?php
-            $defaulPw = "w8woord";
+            $defaulPw = "!pinger65srr";
             $hashedpw = password_hash($defaulPw, PASSWORD_DEFAULT);
 
             #1 verbinding met database
